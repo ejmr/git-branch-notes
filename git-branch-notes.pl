65318afe15f21938d93fad1e42b51d7e788f12a2
Eric James Michael Ritz <lobbyjones@gmail.com>
2012-07-31 16:38:49 -0400

    Create the database filename
    This patch only gives us an absolute path to the SQLite database file
    we will use to store notes.  It does not create the database.

--- git-branch-notes.pl
+++ git-branch-notes.pl
@@ -21,4 +21,22 @@
 use common::sense;
 use DBI;
 
+# Removes all newlines from the given string.  This seems redundant
+# because of chomp() but when parsing the output of Git commands we
+# can end up with newlines in the middle of strings.  So we use this
+# function since chomp() will not remove those.
+sub strip_newlines_from(_) { s/\n//g for $_[0]; }
+
+# We store the notes in an SQLite database inside of the '.git/info'
+# directory at the top-level of a repository.  However, since the
+# program may be run in a sub-directory we need to call git-rev-parse
+# to find out just where the top-level is.
+our $git_info_directory = qx(git rev-parse --show-toplevel) . "/.git/info";
+
+# We have a newline in the middle of our info directory to get rid of.
+strip_newlines_from $git_info_directory;
+
+# The name of our database file.
+our $database_filename = "$git_info_directory/branch-notes.sqlite";
+
 __END__
